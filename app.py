34f82fc46eb347be2efe29a0001bb7bc07fc335c
Deo Andrianto Iskandar <87801747+deoai@users.noreply.github.com>
2024-01-09 00:26:17 +0700

    aa

--- app.py
+++ app.py
@@ -1,6 +1,2 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-from numpy import savetxt
-from PIL import Image, ImageOps
 import tensorflow as tf
+print("Imported TensorFlow successfully")
